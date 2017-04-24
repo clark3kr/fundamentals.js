7cbe8d560547b4c4584ed4d83c142212b17dea2f
clark3kr <clark3kr@dukes.jmu.edu>
2017-04-24 09:40:03 -0400

    Add files via upload

---
+++ fundamentalsBadges.js
@@ -0,0 +1,8 @@
+let age = prompt('What is your age?')
+if (age<=17){
+	console.log('You are too young to use this explicit application')
+} else {
+	let n1 = prompt('Type out a number:')
+	let n2 = prompt('Type out the multiplying number')
+	alert('Your numbers multiplied together are: '+n1*n2)
+}
